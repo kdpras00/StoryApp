8174f5d56569318d87463e9aa3fd63aeaf903835
kdpras00 <kdpras00@gmail.com>
2025-05-30 10:48:52 +0700

    update toogle password ui

--- webpack.common.js
+++ webpack.common.js
@@ -45,6 +45,10 @@ module.exports = {
           from: path.resolve(__dirname, 'src/sw.js'),
           to: path.resolve(__dirname, 'dist'),
         },
+        {
+          from: path.resolve(__dirname, 'src/favicon.ico'),
+          to: path.resolve(__dirname, 'dist'),
+        },
       ],
     }),
   ],
